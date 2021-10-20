34a4021267b6283841e2885ad2b8b9adc958214a
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-10-20 09:01:41 -0300

    adiciona desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,30 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, nome) {
+
+  if (array.length === 5) {
+    array = array.sort();
+  }
+
+  let resultado = '';
+  let novoArray = [];
+  let obj = {
+    tech: array[0],
+    name: nome
+  }
+
+  if (array.length == 0) {
+    resultado = 'Vazio!';
+  } else {
+    for (let a of array) {
+      obj = {
+        tech: a,
+        name: nome
+      }
+      novoArray.push(obj);
+    }
+    resultado = novoArray;
+  }
+  return resultado;
 }
 
 // Desafio 11
